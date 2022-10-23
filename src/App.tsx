569bc15871051aa2ea730d0007a9abfd65a33040
JR DevPM <astronomi@gmail.com>
2022-10-23 16:10:19 -0300

    Use and Import styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
