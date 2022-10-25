13252e8627f809b296841accdc90a9b7ba2e1f6d
JR DevPM <astronomi@gmail.com>
2022-10-25 17:47:33 -0300

    Get STATUS_COLORS dynamic

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -59,8 +59,14 @@ export const HistoryList = styled.div`
   }
 `
 
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
 interface StatusProps {
-  statusColor: 'yellow' | 'red' | 'green'
+  statusColor: keyof typeof STATUS_COLORS
 }
 
 export const Status = styled.span<StatusProps>`
@@ -73,6 +79,6 @@ export const Status = styled.span<StatusProps>`
     width: 0.5rem;
     height: 0.5rem;
     border-radius: 50%;
-    background: ${(props) => props.theme['yellow-500']};
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
