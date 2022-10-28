d9fc40b538a574e5f30016fc4ddb7a730a64c688
JR DevPM <astronomi@gmail.com>
2022-10-28 12:26:16 -0300

    Created accountant pomodoro

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,6 +36,7 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -59,6 +60,17 @@ export function Home() {
 
     reset()
   }
+
+  const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
 
   const isFieldTaskEmpty = watch('task')
   const isFieldMinutesAmountEmpty = watch('minutesAmount')
@@ -90,7 +102,7 @@ export function Home() {
             placeholder="00"
             step={10}
             min={10}
-            // max={60}
+            max={60}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
@@ -98,11 +110,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton
